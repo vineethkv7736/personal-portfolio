5ac715796dea9aff3340e9491af00fe034386d24
vineethkv7736 <vineethkv7736@gmail.com>
2024-08-19 23:03:23 +0530

    import error fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 import { Home } from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/contact'
-import Footer from './components/footer'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
 
