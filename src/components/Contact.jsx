5ab6a6b5283cadaa144c92989c563f1398e9b9ed
vineethkv7736 <vineethkv7736@gmail.com>
2024-08-19 22:59:33 +0530

    contact and footer added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { Meteors } from "./ui/meteors";
-import { ShootingStars } from "./ui/shooting-stars";
-import { StarsBackground } from "./ui/stars-background";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea"
+import { Button } from "./ui/button"
+
+
 const Contact = () => {
   return (
     <section id="#contact" className=" h-screen bg-black">
-        <div>
-            <p className=" text-white"> Contact Me</p>
-           
-        {/* <ShootingStars className="relative h-screen" /> */}
-      <StarsBackground  className="relative h-screen "/>
-     {/* <Meteorss number={20} className="relative" /> */}
+      <div className=" flex justify-center items-center h-screen ">
+        <div className=" w-full relative max-w-xs  md:max-w-sm">
+          <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+          <div className="relative gap-10 shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start text-white ">
+            <div className=" text-2xl md:text-3xl ">
+              <h1>Contact Me</h1>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-5 pl-8 pr-5 w-full">
+              <Input className="bg-slate-300 text-black " placeholder="Name" />
+              <Input className="bg-slate-300 text-black" placeholder="Email" type="email" />
+              <Input className="bg-slate-300 text-black" placeholder="Subject" />
+              <Textarea className="bg-slate-300 text-black" placeholder="Type your message here." />
+              <Button variant="outline" className="bg-slate-300 text-black px-10 md:px-20">Submit</Button>
+
+            </div>
+
+            {/* Meaty part - Meteor effect */}
+            <Meteors number={20} />
+          </div>
         </div>
+      </div>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import { SocialIcon } from 'react-social-icons'
 const Footer = () => {
     return (
-        <section id="#footer" className=" h-20 bg-black">
-                <div className="text-white">
-                    footer
+        <section id="#footer" className=" h-32 bg-black">
+            <div className="p-5 border-t-2 border-blue-300 flex flex-col gap-3 md:gap-5">
+                <div className="text-white text-xl md:text-3xl">
+                    <h1>Connect With Me</h1>
                 </div>
+                <div className="text-white flex flex-row gap-5 pl-5 md:pl-20">
+                <SocialIcon url="https://whatsapp.com" network="whatsapp"  style={{ width: "30px", height: "30px" }} bgColor="#CBD5E1" fgColor="black"/>
+                <SocialIcon url="https://linkedin.com" network="linkedin" style={{ width: "30px", height: "30px" }}  bgColor="#CBD5E1" fgColor="black"/>
+                <SocialIcon url="https://github.com/vineethkv7736" network="github" style={{ width: "30px", height: "30px" }} bgColor="#CBD5E1" fgColor="black"/>
+                </div>
+            </div>
         </section>
     )
 }
